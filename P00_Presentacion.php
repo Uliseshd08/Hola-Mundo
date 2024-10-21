e821105bcedce7a0d5e77595290d5be34287a2d9
Uliseshd08 <ulises26hdez@gmail.com>
2024-10-21 09:29:33 -0600

    Add files via upload

---
+++ P00_Presentacion.php
@@ -0,0 +1,16 @@
+<?php
+/*CBTIS 89
+Programa con mis datos personales
+Avalos Hernandez Ulises Emmanuel
+3-A Programacion*/
+echo "** Presentacion **", "<br>","<br>";
+
+echo "Mi nombre es Avalos Hernandez Ulises Emmanuel";
+echo "<br>";
+echo "Estudio la carrera de programacion en el grupo3-A";
+echo "<br>";
+echo "Mi numero es6183318048";
+echo "<br>";
+echo "Vivo en Durango. Dgo";
+echo "<br>";
+echo "Mi casa esta por Lomas del parque";

0d1b51513057392bb6c5074a396af6a032eeff65
Ulises Hernandez <ulises26hdez@gmail.com>
2024-11-11 10:29:48 -0600

    de ulises
    jfuhuheushus

---
+++ Practicas de el mas vrg, unidad3/22.php
@@ -0,0 +1,58 @@
+<?php
+//p40 array20//
+$array1 = array();
+$array2 = array(); 
+$array3 = array(); 
+
+for ($i = 150; $i <= 201; $i++) {
+    if ($i % 2 == 0) {
+        $array1[] = $i; 
+    } else {
+        $array2[] = $i; 
+    }
+}
+
+
+for ($i = 0; $i < count($array1); $i++) {
+   
+    if (isset($array2[$i])) {
+        $array3[] = $array1[$i] + $array2[$i];
+    } else {
+        $array3[] = $array1[$i]; 
+    }
+}
+echo "CBTIS 89<br>";
+echo "Array1 ------- Array2 ------- Array3<br>";
+
+for ($i = 0; $i < count($array1); $i++) {
+    echo $array1[$i] . " --------- ";
+    echo (isset($array2[$i]) ? $array2[$i] : " ") . " -------- ";
+    echo $array3[$i] . "<br>";
+}
+?>
+
+
+
+
+
+fudgergudrhdrdrgrugrurughuhuuuuhuugurgurgurghrgurrugrghruhhgugdrdrudrugdgrgrgdrgruiiiiiiighhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhfjjjjjjjjjjjjjjjjjjghhhhhhhhhhdkkkkkkkkkkkffffffffhurhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhruuuugrughhghguhiut7578923323w89f8hufhefe yewfewu3w3289527y72yr3r33y9y8933uuuhgsgheruigheuigheuirghraoaerdgarergergugugrnj dhrgereaga
+
+agrgjgjxghdruigjdrjknbvudrgrhgdrf+ffs+
+
+wfuesgdr
+hrhrhr
+hrs
+h
+h
+rshrjhjihjjhijhjrihjdpjhsjpjhfphjerjhrhjjhjdfjfjhidhhirjiorjhioidrihdrihdrhjijhhhhijihdihdiojiiotwwfkk´pkw3ffwklfkfjkkjjjjjdjfbñbbfgg.{gngf.nnnfnr,ñf,,jñfg..n
+htr
+hfg
+htr
+trrtoek6ie4h534uitut98eutydvdkmdfkbvkbkldfbkmbdfkdfkbkfdbmmjfjgjmkmngkmghkmghmngh.mierijirt
+rtrtrrggngtngjtntjg+gtgtr
+gt}ñhtrkkwrettrjyjrtr
+erhtrhte
+htfrhtrlhhtfr
+htrhtrhjrihjtrhrihjtrhirh
+hthrthrhrhjrh
+htrhtrh
